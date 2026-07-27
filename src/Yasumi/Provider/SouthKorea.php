6993dd7e4b392fb7303ba4a9a3c002ecd271540c
Sacha Telgenhof <me@sachatelgenhof.com>
2026-07-27 20:03:38 +0900

    fix(SouthKorea): correct buddhasBirthday translation year bound and update source URL
    - Fix year condition in KoreanTranslation from < 2017 to < 2018 to
      align with Korean Wikipedia: renamed from 석가탄신일 to 부처님오신날
      in October 2017, with the old name applying through the full 2017 year
    - Update VISITKOREA source URL to the correct public holidays page
    
    Signed-off-by: Sacha Telgenhof <me@sachatelgenhof.com>

--- src/Yasumi/Provider/SouthKorea.php
+++ src/Yasumi/Provider/SouthKorea.php
@@ -246,7 +246,7 @@ class SouthKorea extends AbstractProvider
         return [
             'https://en.wikipedia.org/wiki/Public_holidays_in_South_Korea',
             'https://ko.wikipedia.org/wiki/%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD%EC%9D%98_%EA%B3%B5%ED%9C%B4%EC%9D%BC',
-            'https://english.visitkorea.or.kr/enu/TRV/TV_ENG_1_1.jsp',
+            'https://english.visitkorea.or.kr/svc/contents/contentsView.do?vcontsId=140038',
         ];
     }
 
